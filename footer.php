62e9ea584b3ec1b6568915ca07fef7a0b5b76bc8
Jessica Strachan <jessica.strachan@1minus1.com>
2016-11-09 22:32:24 +0000

    Footer Contact section links

--- footer.php
+++ footer.php
@@ -2,6 +2,13 @@
   <div class="footer__top">
     <div class="footer__top--contact">
         <h2>Contact Us</h2>
+        <ul class="footer__top--contact-requests">
+            <li class="footer__top--contact-requests-link"><a href="#">Consultancy Requests</a></li>
+            <li class="footer__top--contact-requests-link"><a href="#">Media Requests</a></li>
+            <li class="footer__top--contact-requests-link"><a href="#">Research Participation</a></li>
+        </ul>
+        <a class="footer__top--contact-email" href="mailto:admin@inclusivemosqueinitiative.org">admin@inclusivemosqueinitiative.org </a>
+        <a class="footer__top--contact-tel" href="tel:+44 203 189 1185 ">+44 203 189 1185</a>
     </div>
     <div class="footer__top--support">
       <h2>Support Us</h2>
