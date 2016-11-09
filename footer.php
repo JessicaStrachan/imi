44db49dd405663140e0e12184bb52f4ee98b456f
JessicaStrachan <jessica.strachan@1minus1.com>
2016-11-09 22:35:20 +0000

    Footer content (#10)
    * Footer Contact section links
    
    * Amend indentation

--- footer.php
+++ footer.php
@@ -1,17 +1,24 @@
-<section class="footer">
-  <div class="footer__top">
-    <div class="footer__top--contact">
-        <h2>Contact Us</h2>
-    </div>
-    <div class="footer__top--support">
-      <h2>Support Us</h2>
-    </div>
-  </div>
-  <div class="footer__bottom">
-    <p>Inclusive Mosque Initiative is a registered charity in England and Wales No. 1161730</p>
-    <p>Inclusive Mosque Initiative - &copy; Copyright 2016</p>
-  </div>
-</section>
+      <section class="footer">
+        <div class="footer__top">
+          <div class="footer__top--contact">
+            <h2>Contact Us</h2>
+            <ul class="footer__top--contact-requests">
+              <li class="footer__top--contact-requests-link"><a href="#">Consultancy Requests</a></li>
+              <li class="footer__top--contact-requests-link"><a href="#">Media Requests</a></li>
+              <li class="footer__top--contact-requests-link"><a href="#">Research Participation</a></li>
+            </ul>
+            <a class="footer__top--contact-email" href="mailto:admin@inclusivemosqueinitiative.org">admin@inclusivemosqueinitiative.org </a>
+            <a class="footer__top--contact-tel" href="tel:+44 203 189 1185 ">+44 203 189 1185</a>
+          </div>
+          <div class="footer__top--support">
+            <h2>Support Us</h2>
+          </div>
+        </div>
+        <div class="footer__bottom">
+          <p>Inclusive Mosque Initiative is a registered charity in England and Wales No. 1161730</p>
+          <p>Inclusive Mosque Initiative - &copy; Copyright 2016</p>
+        </div>
+      </section>
 
     </main>
 
