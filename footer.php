58c3e05307364dc9a47ebb9b8752b75315610561
Jessica Strachan <jessica.strachan@1minus1.com>
2016-11-09 22:07:45 +0000

    Build main bulk of footer

---
+++ footer.php
@@ -0,0 +1,21 @@
+<section class="footer">
+  <div class="footer__top">
+    <div class="footer__top--contact">
+        <h2>Contact Us</h2>
+    </div>
+    <div class="footer__top--support">
+      <h2>Support Us</h2>
+    </div>
+  </div>
+  <div class="footer__bottom">
+    <p>Inclusive Mosque Initiative is a registered charity in England and Wales No. 1161730</p>
+    <p>Inclusive Mosque Initiative - &copy; Copyright 2016</p>
+  </div>
+</section>
+
+    </main>
+
+    <?php wp_footer(); ?>
+
+  </body>
+</html>
