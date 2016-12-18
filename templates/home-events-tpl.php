4aed9e697df861046a756a720ac459b6fa57e952
JessicaStrachan <jessica.strachan@1minus1.com>
2016-12-18 21:10:10 +0000

    Home Featured Event Section (#13)

---
+++ templates/home-events-tpl.php
@@ -0,0 +1,20 @@
+<article class="events">
+  <h2 class="content-title">Upcoming Events</h2>
+  <section class="grid">
+    <div class="grid__col col-3 events-block">
+      <a href="#" class="events-link">
+        <div class="post-image">
+          <div class="hover-overlay"></div>
+        </div>
+        <div class="events-grid__col--title">
+          <h4>Event title</h4>
+        </div>
+        <div class="events-grid__col--info">
+            <time>Satuday 2nd October 11-1pm</time>
+            <p class="address">Westminster Quaker Meeting House, 52 St. Martin’s Lane, WC2N, 4EA</p>
+            <p class="blurb">October’s Feminist Brunch will be a space to share  experiences about being a child or caring for a child who is conscious of gender and how it plays out in their world</p>
+        </div>
+      </a>
+    </div>
+  </section>
+</article>
