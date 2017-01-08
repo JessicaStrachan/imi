cfe6d55ea5ad776cb6739162991b77e58eee30af
JessicaStrachan <hello@jessmadeline.com>
2017-01-08 16:40:31 +0000

    Create About page and Quote template

---
+++ templates/header-quote-tpl.php
@@ -0,0 +1,6 @@
+<section class="quote">
+  <div class="container">
+    <h2 class="quote__title">"This is an example of a quote that will appear on every internal page"</h2>
+    <p class="quote__author">Person who said quote</p>
+  </div>
+</section>
