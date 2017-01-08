e7b08ceb6f00a0deebe45f32bca94a8fc16ecc5b
JessicaStrachan <jstrachan@rawnet.com>
2017-01-08 17:49:10 +0000

    Our Team block (#21)

---
+++ templates/our-team-tpl.php
@@ -0,0 +1,19 @@
+<section class="team">
+  <h2 class="content-title">Our Team</h2>
+  <div class="container">
+    <div class="team__block">
+      <div class="team__block--image">
+      </div>
+      <div class="team__block--info">
+        <h4 class="team__block--name">Joe Bloggs</h4>
+        <div class="team__block--bio">
+          <p>It's ten thousand miles between Kings landing and the wall. The night is dark and full of terrors.</p>
+        </div>
+        <div class="team__block--social">
+          <a class="facebook" href="#"><i class="fa fa-facebook"></i></a>
+          <a class="twitter" href="#"><i class="fa fa-twitter"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
