3880b58f5896aa866daf02f37239b8c96bd89b20
JessicaStrachan <jstrachan@rawnet.com>
2017-01-21 17:31:42 +0000

    Logo (#28)
    * Add header and footer logos
    
    * Remove commented out scss

--- header.php
+++ header.php
@@ -6,7 +6,9 @@
   </head>
   <body>
     <header class="header">
-        <section class="header__logo"></section>
+        <section class="header__logo">
+          <img src="<?php echo get_template_directory_uri(); ?>/public/images/imi-logo-header.png" alt="Imi Logo"/>
+        </section>
         <section class="navigation">
             <?php get_template_part('templates/navigation', 'tpl'); ?>
         </section>
