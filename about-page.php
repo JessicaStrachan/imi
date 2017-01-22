0208f42fefe89dd8c41e4c27dbaab4866c11cfd8
JessicaStrachan <hello@jessmadeline.com>
2017-01-22 20:01:11 +0000

    Add quote section to about page

--- about-page.php
+++ about-page.php
@@ -4,10 +4,19 @@
 */
 get_header();
 
-get_template_part('templates/header-quote', 'tpl');
-get_template_part('templates/two-block', 'tpl');
-get_template_part('templates/our-team', 'tpl');
+$about = new CMB2Fields(get_the_ID());
+
+$post_image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large');
 ?>
 
+<section class="quote" style="background-image: url('<?php echo $post_image[0]; ?>')">
+  <div class="container">
+    <h2 class="quote__title"><?php echo $about->format_content($about->field('about_quote_text')); ?></h2>
+    <h2 class="quote__author"><?php echo $about->format_content($about->field('about_quote_author_text')); ?></h2>
+  </div>
+</section>
+
 <?php
+get_template_part('templates/two-block', 'tpl');
+get_template_part('templates/our-team', 'tpl');
 get_footer();
