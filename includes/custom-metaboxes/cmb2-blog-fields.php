366f36e72c2dfd6a24083d867ef5fe625aa9cf61
JessicaStrachan <hello@jessmadeline.com>
2017-01-22 19:43:49 +0000

    fix conflicts

--- includes/custom-metaboxes/cmb2-blog-fields.php
+++ includes/custom-metaboxes/cmb2-blog-fields.php
@@ -3,11 +3,7 @@
 $blog_quote = new_cmb2_box([
   'id' => 'blog_quote',
   'title' => __('Blog Quote', 'imi'),
-<<<<<<< HEAD
   'object_types' => ['post'],
-=======
-  'object_types' => ['blog'],
->>>>>>> 676d63a0c16f699c2d27f8fd9a2401c722b5a04d
   'context' => 'normal',
   'priority' => 'high',
   'show_names' => true
@@ -19,20 +15,7 @@ $blog_quote->add_field([
   'type' => 'text'
 ]);
 
-<<<<<<< HEAD
 $blog_quote->add_field([
-=======
-$blog_quote_author = new_cmb2_box([
-  'id' => 'blog_quote_author',
-  'title' => __('Blog Quote Author', 'imi'),
-  'object_types' => ['blog'],
-  'context' => 'normal',
-  'priority' => 'high',
-  'show_names' => true
-]);
-
-$blog_quote_author->add_field([
->>>>>>> 676d63a0c16f699c2d27f8fd9a2401c722b5a04d
   'desc' => __('Enter text to be displayed as author of quote'),
   'id' => $prefix . 'blog_quote_author_text',
   'type' => 'text'
