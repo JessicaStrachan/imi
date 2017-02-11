9012c6b15d37f0352f98ab00e01dad5f46a535e5
JessicaStrachan <hello@jessmadeline.com>
2017-02-11 11:16:45 +0000

    Featured Events - needs hooking up to select featured events

--- templates/home-events-tpl.php
+++ templates/home-events-tpl.php
@@ -1,20 +1,41 @@
-<article class="events">
-  <h2 class="content-title">Upcoming Events</h2>
-  <section class="grid">
-    <div class="grid__col col-3 events-block">
-      <a href="#" class="events-link">
-        <div class="post-image">
-          <div class="hover-overlay"></div>
+<?php
+
+$events = new Query('events');
+$event = new CMB2Fields(get_the_ID());
+$query_args = [
+  'posts_per_page' => -1
+];
+$results = $events->query($query_args);
+?>  
+
+<div class="container">
+  <h1 class="content-title content-title--light-red">Events</h1>
+  <ul class="grid">
+
+    <?php
+    if($results->have_posts()): while($results->have_posts()): $results->the_post();
+      $event_fields = new CMB2Fields(get_the_ID());
+    ?>
+
+    <li class="card grid__col col-4">
+      <a class="card__link" href="<?php the_permalink(); ?>">
+
+        <?php $post_image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large'); ?>
+
+        <div class="card__media" style="background-image: url('<?php echo $post_image[0]; ?>')"></div>
+        <div class="card__info">
+          <h2 class="heading--two heading--bold"><?php the_title(); ?></h2>
+          <h3 class="heading--three heading--bold"><?php echo $event->format_content($event->field('event_date_text')); ?></h3>
+          <p><?php echo $event->format_content($event->field('event_address_text')); ?></p>
+          <p class="card__excerpt"><?php echo $event->format_content($event->field('event_excerpt_text')); ?></p>
+          <p class="card__read-more">Read More</p>
         </div>
-        <div class="events-grid__col--title">
-          <h4>Event title</h4>
-        </div>
-        <div class="events-grid__col--info">
-            <time>Satuday 2nd October 11-1pm</time>
-            <p class="address">Westminster Quaker Meeting House, 52 St. Martin’s Lane, WC2N, 4EA</p>
-            <p class="blurb">October’s Feminist Brunch will be a space to share  experiences about being a child or caring for a child who is conscious of gender and how it plays out in their world</p>
-        </div>
+
       </a>
-    </div>
-  </section>
-</article>
+    </li>
+
+    <?php endwhile; endif; ?>
+
+  </ul>
+</div>
+
