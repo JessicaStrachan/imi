acff714309df735ff332d480343a1493839e092f
JessicaStrachan <jstrachan@rawnet.com>
2017-03-04 16:07:05 +0000

    mobile nav (#45)

--- templates/navigation-tpl.php
+++ templates/navigation-tpl.php
@@ -1,8 +1,12 @@
-<ul class="header__navigation">
-  <li class="header__navigation--item"><a href="#">About</a></li>
-  <li class="header__navigation--item"><a href="#">Advice</a></li>
-  <li class="header__navigation--item"><a href="#">Events</a></li>
-  <li class="header__navigation--item"><a href="#">Donate</a></li>
-  <li class="header__navigation--item"><a href="#">Blog</a></li>
-  <li class="header__navigation--item"><a href="#">Contact</a></li>
+<ul class="navigation js-nav-menu">
+  <li class="navigation__item"><a class="navigation__link" href="#">About</a></li>
+  <li class="navigation__item"><a class="navigation__link" href="#">Advice</a></li>
+  <li class="navigation__item"><a class="navigation__link" href="#">Events</a></li>
+  <li class="navigation__item"><a class="navigation__link" href="#">Donate</a></li>
+  <li class="navigation__item"><a class="navigation__link" href="#">Blog</a></li>
+  <li class="navigation__item"><a class="navigation__link" href="#">Contact</a></li>
 </ul>
+
+<div class="menu-icon js-menu-icon">
+	<span class="menu-bar"></span>
+</div>
