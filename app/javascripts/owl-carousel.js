14f7d6817f3ed09ea2c4e5293373084ccb09d885
JessicaStrachan <jstrachan@rawnet.com>
2017-05-11 21:57:34 +0100

    homepage owl carousel (#56)

---
+++ app/javascripts/owl-carousel.js
@@ -0,0 +1,11 @@
+(function($) {
+
+ $('.owl-carousel').owlCarousel({
+   autoHeight: true,
+   autoplay: false,
+   autoplayTimeout: 5000,
+   dots: true,
+   items: 1,
+   mouseDrag: true
+ });
+}(jQuery));
