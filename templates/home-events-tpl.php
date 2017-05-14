3779ba2d4062744749558ee0c053c1fb213bd821
JessicaStrachan <jstrachan@rawnet.com>
2017-05-14 20:28:14 +0100

    more styling amends (#57)

--- templates/home-events-tpl.php
+++ templates/home-events-tpl.php
@@ -9,7 +9,7 @@ $results = $events->query($query_args);
 ?>
 
 <div class="container">
-  <h1 class="heading--one heading--bold content-title content-title--light-red">Events</h1>
+  <h1 class="heading--one heading--bold content-title content-title--light-red">Upcoming Events</h1>
   <ul class="grid">
 
     <?php
