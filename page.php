dd306665a95a2c730dc06f61c6eeb81eb64860c5
JessicaStrachan <hello@jessmadeline.com>
2017-07-01 23:13:16 +0100

    Remove date from default page template

--- page.php
+++ page.php
@@ -19,11 +19,6 @@ $post_image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'lar
   <section class="post__title">
     <div class="container">
       <h1 class="heading--one heading--bold heading--primary heading--asset u-align-center"><?php the_title(); ?></h1>
-      <h5 class="post__title--date">
-        <time datetime="<?php echo get_the_time('Y-m-d'); ?>">
-          <?php echo get_the_time('F j Y'); ?>
-        </time>
-      </h5>
     </div>
   </section>
   <section class="post__content">
