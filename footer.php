71ced35fe718537c06830c8db4d19c4d4c69591b
Jessica Strachan <hello@jessmadeline.com>
2017-07-04 21:03:45 +0100

    Social footer links and post links and list styling

--- footer.php
+++ footer.php
@@ -1,23 +1,20 @@
       <section class="footer">
         <div class="footer__top">
           <div class="footer__top--contact">
-            <h2 class="heading--two heading--bold">Contact Us</h2>
-            <ul class="footer__top--contact-requests">
-              <li class="footer__top--contact-requests-link"><a href="/contact">Contact</a></li>
-            </ul>
+            <h2 class="heading--two heading--bold"><a class="heading__link" href="/contact">Contact Us</a></h2>
             <a class="footer__top--contact-tel" href="tel:+44 203 189 1185">+44 203 189 1185</a>
             <a class="footer__top--contact-email" href="mailto:admin@inclusivemosqueinitiative.org">admin@inclusivemosqueinitiative.org </a>
             <div class="footer__top--social">
-              <a class="footer__top--social-link" href="#"><i class="fa fa-facebook"></i></a>
-              <a class="footer__top--social-link" href="#"><i class="fa fa-twitter"></i></a>
-              <a class="footer__top--social-link" href="#"><i class="fa fa-skype"></i></a>
-              <a class="footer__top--social-link paypal-link" href="https://www.paypal.com/uk/cgi-bin/webscr?cmd=_flow&SESSION=xVevG1MrHyVz395Tgsjb5hmcdbp05nVAB6QIYcx2qKVomkYCD7fx2oQC9WS&dispatch=5885d80a13c0db1f8e263663d3faee8d83a0bf7db316a7beb1b14b43acd04037&rapidsState=Donation__DonationFlow___StateDonationBilling&rapidsStateSignature=34846b7fae7e4369413be25a6c09b0fdf7e7b1ef" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/public/images/paypal-donate.jpg"></a>
+              <a class="footer__top--social-link" target"_blank" href="https://www.facebook.com/inclusivemosqueinitiative"><i class="fa fa-facebook"></i></a>
+              <a class="footer__top--social-link" target"_blank" href="https://twitter.com/inclusivemosque"><i class="fa fa-twitter"></i></a>
+              <a class="footer__top--social-link" target"_blank" href="#"><i class="fa fa-skype"></i></a>
+              <a class="footer__top--social-link paypal-link" href="https://www.paypal.com/uk/cgi-bin/webscr?cmd=_flow&SESSION=xVevG1MrHyVz395Tgsjb5hmcdbp05nVAB6QIYcx2qKVomkYCD7fx2oQC9WS&dispatch=5885d80a13c0db1f8e263663d3faee8d83a0bf7db316a7beb1b14b43acd04037&rapidsState=Donation__DonationFlow___StateDonationBilling&rapidsStateSignature=34846b7fae7e4369413be25a6c09b0fdf7e7b1ef" target="_blank"><i class="fa fa-paypal"></i></a>
             </div>
           </div>
           <div class="footer__top--support">
             <h2 class="heading--two heading--bold">Support Us</h2>
             <h4 class="heading--bold">Please join our mailing list. All information is kept confidential and your identity is not shared with anyone else.</h4>
-            <?php echo do_shortcode("[mc4wp_form id='21']"); ?>
+            <?php echo do_shortcode("[mc4wp_form id='6617']"); ?>
           </div>
         </div>
         <div class="footer__bottom">
