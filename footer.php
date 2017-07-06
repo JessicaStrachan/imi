eb411649d881cc3a483e876a4e5ce0b3023c8951
Jessica Strachan <hello@jessmadeline.com>
2017-07-06 21:56:03 +0100

    Remove skype link, update font styling

--- footer.php
+++ footer.php
@@ -7,7 +7,6 @@
             <div class="footer__top--social">
               <a class="footer__top--social-link" target"_blank" href="https://www.facebook.com/inclusivemosqueinitiative"><i class="fa fa-facebook"></i></a>
               <a class="footer__top--social-link" target"_blank" href="https://twitter.com/inclusivemosque"><i class="fa fa-twitter"></i></a>
-              <a class="footer__top--social-link" target"_blank" href="#"><i class="fa fa-skype"></i></a>
               <a class="footer__top--social-link paypal-link" href="https://www.paypal.com/uk/cgi-bin/webscr?cmd=_flow&SESSION=xVevG1MrHyVz395Tgsjb5hmcdbp05nVAB6QIYcx2qKVomkYCD7fx2oQC9WS&dispatch=5885d80a13c0db1f8e263663d3faee8d83a0bf7db316a7beb1b14b43acd04037&rapidsState=Donation__DonationFlow___StateDonationBilling&rapidsStateSignature=34846b7fae7e4369413be25a6c09b0fdf7e7b1ef" target="_blank"><i class="fa fa-paypal"></i></a>
             </div>
           </div>
