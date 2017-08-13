74cc33c2299930aa4d44a5740ce4237fb30800c2
Jessica Strachan <jstrachan@rawnet.com>
2017-08-13 21:49:33 +0100

    fix layout of 404 page (#70)

--- 404.php
+++ 404.php
@@ -4,17 +4,17 @@ get_header();
 ?>
 
 <article class="container">
-  <section class="">
+  <section class="message-404">
     <h1 class="heading--one heading--bold heading--primary heading--asset u-align-center">404</h1>
-  </section>
-  <section class="404-message">
-    <p class="404-message__text">The page you are looking for could not be found.<br>
+    <p class="message-404__text">The page you are looking for could not be found.<br>
       Please return to our home page or</p>
-    <a href="/contact/" class="btn btn--secondary 404-message__button">contact us</a></p>
-    <a href="<?php echo get_site_url(); ?>" class="btn btn--secondary 404-message__button" data-text="Home page">Home
+    <div class="flex flex--justify-center">
+      <a href="/contact/" class="message-404__button">contact us</a></p>
+      <a href="<?php echo get_site_url(); ?>" class="message-404__button" data-text="Home page">Home
+    </a>
     </a>
   </section>
 </article>
-
+        
 <?php
 get_footer();
