adc13366c9de71c8defc41c13bb4205c8637def0
Jessica Strachan <hello@jessmadeline.com>
2017-09-17 21:32:59 +0100

    Contact page Quote fields

--- contact.php
+++ contact.php
@@ -4,15 +4,15 @@
 */
 get_header();
 
-$page = new CMB2Fields(get_the_ID());
+$contact_page = new CMB2Fields(get_the_ID());
 
 $post_image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large');
 ?>
 
 <section class="quote" style="background-image: url('<?php echo $post_image[0]; ?>')">
   <div class="container">
-    <h2 class="quote__title"><?php echo $page->format_content($page->field('page_quote_text')); ?></h2>
-    <h2 class="quote__author"><?php echo $page->format_content($page->field('page_quote_author_text')); ?></h2>
+    <h2 class="quote__title"><?php echo $contact_page->format_content($contact_page->field('contact_quote_text')); ?></h2>
+    <h2 class="quote__author"><?php echo $contact_page->format_content($contact_page->field('contact_quote_author_text')); ?></h2>
   </div>
 </section>
 
