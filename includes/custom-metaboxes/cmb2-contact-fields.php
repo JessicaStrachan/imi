6b5c26c36e95dd7ccc0e5b249a3c9855dfd824fe
Jessica Strachan <jstrachan@rawnet.com>
2017-12-16 11:46:26 +0000

    Media requests form (#83)
    * add margin between text and subscribe mail input
    
    * --wip-- [skip ci]
    
    * --wip-- [skip ci]
    
    * Contact Page changes with ninja forms and remove tabs

--- includes/custom-metaboxes/cmb2-contact-fields.php
+++ includes/custom-metaboxes/cmb2-contact-fields.php
@@ -36,3 +36,61 @@ $contact_us_quote_author->add_field([
   'id' => $prefix . 'contact_quote_author_text',
   'type' => 'text'
 ]);
+
+$contact_us_contact_form = new_cmb2_box([
+  'id' => 'contact_us_contact_form',
+  'title' => __('contact Us Contact Form', 'imi'),
+  'object_types' => ['page'],
+  'show_on'      => [
+    'key'   => 'page-template',
+    'value' => 'contact.php'
+  ],
+  'context' => 'normal',
+  'priority' => 'high',
+  'show_names' => true
+]);
+
+$contact_us_contact_form->add_field([
+  'desc' => __('Enter contact form shortcode'),
+  'id' => $prefix . 'contact_us_contact_shortcode',
+  'type' => 'wysiwyg'
+]);
+
+// Not needed until post launch
+// $contact_us_media_form = new_cmb2_box([
+//   'id' => 'contact_us_contact_form',
+//   'title' => __('Contact Us Media Form', 'imi'),
+//   'object_types' => ['page'],
+//   'show_on'      => [
+//     'key'   => 'page-template',
+//     'value' => 'contact.php'
+//   ],
+//   'context' => 'normal',
+//   'priority' => 'high',
+//   'show_names' => true
+// ]);
+
+// $contact_us_media_form->add_field([
+//   'desc' => __('Enter Media form shortcode'),
+//   'id' => $prefix . 'contact_us_media_shortcode',
+//   'type' => 'wysiwyg'
+// ]);
+
+// $contact_us_research_form = new_cmb2_box([
+//   'id' => 'contact_us_contact_form',
+//   'title' => __('Contact Us Research Form', 'imi'),
+//   'object_types' => ['page'],
+//   'show_on'      => [
+//     'key'   => 'page-template',
+//     'value' => 'contact.php'
+//   ],
+//   'context' => 'normal',
+//   'priority' => 'high',
+//   'show_names' => true
+// ]);
+
+// $contact_us_research_form->add_field([
+//   'desc' => __('Enter Research form shortcode'),
+//   'id' => $prefix . 'contact_us_research_shortcode',
+//   'type' => 'wysiwyg'
+// ]);
