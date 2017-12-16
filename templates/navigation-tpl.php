10bfa8f799d83a3fb889d3a45eb2e7ec57493287
Jessica Strachan <jstrachan@rawnet.com>
2017-12-16 14:42:32 +0000

    Finishing touches (#84)
    * add margin between text and subscribe mail input
    
    * finishing touches from soofiya

--- templates/navigation-tpl.php
+++ templates/navigation-tpl.php
@@ -1,6 +1,5 @@
 <ul class="navigation js-nav-menu">
   <li class="navigation__item"><a class="navigation__link" href="/about">About</a></li>
-  <li class="navigation__item"><a class="navigation__link" href="/advice">Advice</a></li>
   <li class="navigation__item"><a class="navigation__link" href="/events">Events</a></li>
   <li class="navigation__item"><a class="navigation__link" target="_blank" href="https://inclusivemosqueinitiative.charitycheckout.co.uk/">Donate</a></li>
   <li class="navigation__item"><a class="navigation__link" href="/blog">Blog</a></li>
