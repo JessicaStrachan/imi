433deb4a022d48c4320e18df069ca94bdf60c119
Jessica Strachan <hello@jessmadeline.com>
2018-02-07 21:31:50 +0000

    Tabs on Contact page

--- contact.php
+++ contact.php
@@ -2,64 +2,6 @@
 /**
 * Template Name: Contact page
 */
-
-//response generation function
-$response = "";
-
-//function to generate response
-function my_contact_form_generate_response($type, $message){
-
-global $response;
-
-if($type == "success") $response = "<div class='success'><p>{$message}</p></div>";
-else $response = "<div class='error'><p>{$message}</p></div>";
-
-}
-
-//response messages
-$not_human       = "Human verification incorrect.";
-$missing_content = "Please supply all information.";
-$email_invalid   = "Email Address Invalid.";
-$message_unsent  = "Message was not sent. Try Again.";
-$message_sent    = "Thanks! Your message has been sent.";
-
-//user posted variables
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$email = $_POST['email'];
-$telephone = $_POST['telephone'];
-$message = $_POST['message'];
-$human = $_POST['human'];
-
-//php mailer variables
-$to = get_option('admin_email');
-$subject = $first_name . " " . $last_name . ", " . $telephone . ", sent a message from ".get_bloginfo('name');
-$headers = 'From: '. $email . "\r\n" . 'Reply-To: ' . $email . "\r\n";
-
-if(!$human == 0){
-  if($human != 2) my_contact_form_generate_response("error", $not_human); //not human!
-  else {
-
-    //validate email
-    if(!filter_var($email, FILTER_VALIDATE_EMAIL))
-      my_contact_form_generate_response("error", $email_invalid);
-    else //email is valid
-    {
-        //validate presence of name and message
-        if(empty($first_name) || empty($message) || empty($last_name)){
-          my_contact_form_generate_response("error", $missing_content);
-        }
-        else //ready to go!
-        {
-            $sent = wp_mail($to, $subject, strip_tags($message), $headers);
-            if($sent) my_contact_form_generate_response("success", $message_sent); //message sent!
-            else my_contact_form_generate_response("error", $message_unsent); //message wasn't sent
-        }
-    }
-  }
-}
-else if ($_POST['submitted']) my_contact_form_generate_response("error", $missing_content);
-
 get_header();
 
 $contact_page = new CMB2Fields(get_the_ID());
@@ -81,18 +23,22 @@ $post_image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'lar
 			<a class="address__link" href="mailto:admin@inclusivemosqueinitiative.org">admin@inclusivemosqueinitiative.org</a>
 		</div>
 	</div>
-	<!-- <div class="form-tabs">
-    <ul>
-		  <button class="form-tabs__button btn--tabs btn btn--secondary js-contact-tab tab-active" data-behaviour="toggle-button" href="#contact">Contact</button>
-      <button class="form-tabs__button btn--tabs btn btn--secondary js-media-tab" data-behaviour="toggle-button" href="#media">Media Requests</button>
-    </ul>
-	</div> -->
-	<div class="central-paragraph">
+  <div class="central-paragraph">
 		<p>If you have any queries, media, press or just want to get in touch please contact us through the form below. We’ll endeavour to get back to you as soon as possible.</p>
 	</div>
+	<div class="form-tabs">
+    <ul>
+		  <button class="form-tabs__button btn--tabs btn btn--secondary js-contact-tab tab-active" data-behaviour="toggle-button" href="#researchers">Research Requests</button>
+      <button class="form-tabs__button btn--tabs btn btn--secondary js-media-tab" data-behaviour="toggle-button" href="#media">Media Requests</button>
+    </ul>
+	</div>
 
-    <section class="form form--active" id="contact" data-behaviour="toggle-content">
+    <section class="form form--active" id="researchers" data-behaviour="toggle-content">
         <?php echo $contact_page->format_content($contact_page->field('contact_us_contact_shortcode')); ?>
+    </section>
+
+    <section class="form" id="media" data-behaviour="toggle-content">
+      <?php echo $contact_page->format_content($contact_page->field('contact_us_media_shortcode')); ?>
     </section>
 
 </section>
--- includes/custom-metaboxes/cmb2-contact-fields.php
+++ includes/custom-metaboxes/cmb2-contact-fields.php
@@ -39,7 +39,7 @@ $contact_us_quote_author->add_field([
 
 $contact_us_contact_form = new_cmb2_box([
   'id' => 'contact_us_contact_form',
-  'title' => __('contact Us Contact Form', 'imi'),
+  'title' => __('Research Form Shortcode and Tab content', 'imi'),
   'object_types' => ['page'],
   'show_on'      => [
     'key'   => 'page-template',
@@ -56,25 +56,24 @@ $contact_us_contact_form->add_field([
   'type' => 'wysiwyg'
 ]);
 
-// Not needed until post launch
-// $contact_us_media_form = new_cmb2_box([
-//   'id' => 'contact_us_contact_form',
-//   'title' => __('Contact Us Media Form', 'imi'),
-//   'object_types' => ['page'],
-//   'show_on'      => [
-//     'key'   => 'page-template',
-//     'value' => 'contact.php'
-//   ],
-//   'context' => 'normal',
-//   'priority' => 'high',
-//   'show_names' => true
-// ]);
+$contact_us_media_form = new_cmb2_box([
+  'id' => 'contact_us_media_form',
+  'title' => __('Media Form and Tab Content', 'imi'),
+  'object_types' => ['page'],
+  'show_on'      => [
+    'key'   => 'page-template',
+    'value' => 'contact.php'
+  ],
+  'context' => 'normal',
+  'priority' => 'high',
+  'show_names' => true
+]);
 
-// $contact_us_media_form->add_field([
-//   'desc' => __('Enter Media form shortcode'),
-//   'id' => $prefix . 'contact_us_media_shortcode',
-//   'type' => 'wysiwyg'
-// ]);
+$contact_us_media_form->add_field([
+  'desc' => __('Enter Media form shortcode'),
+  'id' => $prefix . 'contact_us_media_shortcode',
+  'type' => 'wysiwyg'
+]);
 
 // $contact_us_research_form = new_cmb2_box([
 //   'id' => 'contact_us_contact_form',
