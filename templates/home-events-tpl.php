729a90b66c407ffee25ef81c93d8a660ffd99142
Jessica Strachan <hello@jessmadeline.com>
2018-02-15 20:50:48 +0000

    post width, backgrounds on home cards

--- templates/home-events-tpl.php
+++ templates/home-events-tpl.php
@@ -17,20 +17,22 @@ $results = $events->query($query_args);
     ?>
 
     <li class="card grid__col col-4">
-      <a class="card__link" href="<?php the_permalink(); ?>">
+      <div <?php post_class() ?> >
+        <a class="card__link" href="<?php the_permalink(); ?>">
 
-        <?php $post_image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large'); ?>
+          <?php $post_image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large'); ?>
 
-        <div class="card__media" style="background-image: url('<?php echo $post_image[0]; ?>')"></div>
-        <div class="card__info">
-          <h2 class="card__title"><?php the_title(); ?></h2>
-          <p class="card__details"><?php echo $event->field('event_date_text'); ?>
-          <?php echo $event->field('event_address_text'); ?></p>
-          <p class="card__excerpt"><?php echo $event->format_content($event->field('event_excerpt_text')); ?></p>
-          <p class="card__read-more">Read More</p>
-        </div>
+          <div class="card__media" style="background-image: url('<?php echo $post_image[0]; ?>')"></div>
+          <div class="card__info">
+            <h2 class="card__title"><?php the_title(); ?></h2>
+            <p class="card__details"><?php echo $event->field('event_date_text'); ?>
+            <?php echo $event->field('event_address_text'); ?></p>
+            <p class="card__excerpt"><?php echo $event->format_content($event->field('event_excerpt_text')); ?></p>
+            <p class="card__read-more">Read More</p>
+          </div>
 
-      </a>
+        </a>
+      </div>
     </li>
 
     <?php endwhile; endif; ?>
