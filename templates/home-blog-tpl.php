595ccb1705498ad40dbf1e5f884640ff46c4f52d
Jessica Strachan <jstrachan@rawnet.com>
2018-02-15 21:40:57 +0000

    Remove custom list styling (#96)
    * Remove custom list styling
    
    * post width, backgrounds on home cards
    
    * text wrap image classes

--- templates/home-blog-tpl.php
+++ templates/home-blog-tpl.php
@@ -20,19 +20,21 @@ endforeach;
   <?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
 
     <li class="card grid__col col-4">
-      <a class="card__link" href="<?php the_permalink(); ?>">
+      <div <?php post_class() ?> >
+        <a class="card__link" href="<?php the_permalink(); ?>">
 
-        <?php $post_image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large'); ?>
+          <?php $post_image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large'); ?>
 
-        <div class="card__media" style="background-image: url('<?php echo $post_image[0]; ?>')"></div>
-        <div class="card__info">
-          <h2 class="card__title"><?php the_title(); ?></h2>
-          <?php the_excerpt(); ?>
+          <div class="card__media" style="background-image: url('<?php echo $post_image[0]; ?>')"></div>
+          <div class="card__info">
+            <h2 class="card__title"><?php the_title(); ?></h2>
+            <?php the_excerpt(); ?>
 
-          <p class="card__read-more">Read More</p>
-        </div>
+            <p class="card__read-more">Read More</p>
+          </div>
 
-      </a>
+        </a>
+      </div>
     </li>
 
     <?php endwhile; endif; ?>
